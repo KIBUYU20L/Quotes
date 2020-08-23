3cef64a6ead79d2d17d3b5393f55d9b17fccdb9d
KIBUYU20L <oliverotiato283@gmail.com>
2020-08-23 11:21:30 +0300

    add a quote class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Qoutes'
-  quote = 'Watch Finding Nemo'
+  title = 'Quotes'
+  quotes:Quote[] = [
+    {id:1, name:'Once bitten twice shy'},
+    {id:2,name:'No man is an Island'},
+    {id:3,name:'God is the way'},
+    {id:4,name:'Act as if what you do makes a difference. Yes It Does.'},
+    // {id:5,name:'Solve math homework'},
+    // {id:6,name:'Plot my world domination plan'},
+  ];
 }
