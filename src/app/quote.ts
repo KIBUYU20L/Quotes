0fbbef0ae2da4025bb8e05606b0a692288dedd43
KIBUYU20L <oliverotiato283@gmail.com>
2020-08-24 06:44:52 +0300

    pipes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    showQuote: boolean;
-  constructor(public id: number,public name: string,public quote: string){
+    public showQuote: boolean;
+  constructor(public id: number,public name: string,public quote: string,  public completeDate: Date){
     this.showQuote=false;
   }
 }
