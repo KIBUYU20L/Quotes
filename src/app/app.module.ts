9912551d1ab3cab117073597efefd5c46c7abaa6
KIBUYU20L <oliverotiato283@gmail.com>
2020-08-24 07:38:17 +0300

    create a quote-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteDetailComponent,
     StrikethroughDirective,
     DateCountPipe,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
